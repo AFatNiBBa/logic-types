86d30c2520c3f9fdda415cd6f340e006ab2c8771
s.alunni <sean.alunni@gbsoftware.it>
2024-01-11 10:17:36 +0100

    Ottimizzati un po' di tipi e tolto `Head` perchè era ottimizzabile in 4 caratteri (Ci voleva più a chiamallo che a definillo)

--- src/lib/tuple.ts
+++ src/lib/tuple.ts
@@ -6,19 +6,13 @@ import { Ternary } from "./gates";
  * Returns the length of a tuple
  * @template T The tuple from which to get the length
  */
-export type Length<T extends readonly unknown[]> = T extends { length: infer U } ? number & U : never;
-
-/**
- * Returns the first element of a tuple
- * @template T The tuple from which to return the first element
- */
-export type Head<T extends readonly unknown[]> = T extends readonly [ infer U, ...unknown[] ] ? U : never;
+export type Length<T extends readonly unknown[]> = T["length"] & number;
 
 /**
  * Returns the last element of a tuple
  * @template T The tuple from which to return the last element
  */
-export type Tail<T extends readonly unknown[]> = T extends readonly [ infer U ] ? U : T extends readonly [ unknown, ...infer U ] ? Tail<U> : never;
+export type Tail<T extends readonly unknown[]> = T extends readonly [ ...unknown[], infer U ] ? U : never;
 
 /**
  * Creates a tuple of {@link T} with length {@link N} and starts with the elements from {@link Prev}
@@ -26,20 +20,18 @@ export type Tail<T extends readonly unknown[]> = T extends readonly [ infer U ] 
  * @template T The type of each new added element
  * @template Prev The tuple on which elements will be added until the length becomes {@link N}
  */
-export type Alloc<N extends number, T = unknown, Prev extends unknown[] = []> = Prev extends { length: N } ? Prev : Alloc<N, T, [ ...Prev, T ]>;
+export type Alloc<N extends number, T = unknown, Prev extends readonly unknown[] = []> = Prev extends { length: N } ? Prev : Alloc<N, T, [ ...Prev, T ]>;
 
 /**
  * Flattens a tuple of tuples
  * @template T The tuple to flatten
  */
 export type Flat<T extends readonly unknown[][]> =
-    T extends readonly [] 
-        ? []
-        : T extends readonly [ [ ...infer U ], ...infer Rest ]
-            ? Rest extends unknown[][]
-                ? [ ...U, ...Flat<Rest> ]
-                : never
-            : never; 
+    T extends readonly [ readonly [ ...infer U ], ...infer Rest ]
+        ? Rest extends readonly unknown[][]
+            ? [ ...U, ...Flat<Rest> ]
+            : never
+        : [];
 
 /**
  * Filters an array's elements based on a predicate.
@@ -48,8 +40,6 @@ export type Flat<T extends readonly unknown[][]> =
  * @template P A function that takes an element whether or not it should be present on the result
  */
 export type FilterArray<T extends readonly unknown[], P extends (v: any) => boolean> = 
-    T extends readonly [] 
-        ? [] 
-        : T extends readonly [ infer U, ...infer Rest ] 
-            ? [ ...Ternary<Check<P, [ U ]>, [ U ], []>, ...FilterArray<Rest, P> ] 
-            : never;
+    T extends readonly [ infer U, ...infer Rest ] 
+        ? [ ...Ternary<Check<P, [ U ]>, [ U ], []>, ...FilterArray<Rest, P> ] 
+        : [];
